b7c95dc28ef7effdeb31c2b08e5dab747684ed9e
Priti Kumari <151085priti@gmail.com>
2025-05-09 16:09:25 +0800

    Intial Commit

---
+++ src/main/java/com/priti/iitd/java8/scenarioBased/Java8ScenarioBasedApplication.java
@@ -0,0 +1,13 @@
+package com.priti.iitd.java8.scenarioBased;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8ScenarioBasedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8ScenarioBasedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/priti/iitd/java8/scenarioBased/Java8ScenarioBasedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priti.iitd.java8.scenarioBased;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8ScenarioBasedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
